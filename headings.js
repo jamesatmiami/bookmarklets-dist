1fef136b928bac17d7b8cd3771922f657a905f11
lennonpc <lennonpc@miamioh.edu>
2023-01-05 12:31:19 -0500

    removed aria-level, seeing if role heading works

--- headings.js
+++ headings.js
@@ -43,7 +43,7 @@
                 color: "brown",
                 label: "h6"
             }, {
-                selector: "[role=heading aria-level=1]",
+                selector: "[role=heading]",
                 color: "navy",
                 label: "h1"
             }],
